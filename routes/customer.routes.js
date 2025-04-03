33d1b139862fbb809aac87cbcf8b2508917fc91b
Natali <natalizapata869@gmail.com>
2025-04-03 13:01:31 -0500

    update files sales and customers

--- routes/customer.routes.js
+++ routes/customer.routes.js
@@ -1,16 +1,18 @@
-import { Router } from "express";
-import { check } from "express-validator";
-import { getCustomers, createCustomer, updateCustomer, deleteCustomer } from "../controllers/customer.controller.js";
+import express from "express";
+import {
+    getCustomers,
+    getCustomerById,
+    createCustomer,
+    updateCustomer,
+    deleteCustomer
+} from "../controllers/customer.controller.js";
+
 import { authenticateUser, authorizePermission } from "../middlewares/auth.middleware.js";
 
-const router = Router();
+const router = express.Router();
 
-const customerValidations = [
-    check("name").notEmpty().withMessage("Name is required"),
-    check("email").isEmail().withMessage("Valid email is required"),
-    check("phone").notEmpty().withMessage("Phone is required")
-];
 router.get("/", authenticateUser, authorizePermission("view_customers"), getCustomers);
+router.get("/:id", authenticateUser, authorizePermission("view_customers_id"), getCustomerById);
 router.post("/", authenticateUser, authorizePermission("create_customers"), createCustomer);
 router.put("/:id", authenticateUser, authorizePermission("update_customers"), updateCustomer);
 router.delete("/:id", authenticateUser, authorizePermission("delete_customers"), deleteCustomer);
--- utils/permissions.js
+++ utils/permissions.js
@@ -6,16 +6,16 @@ export const checkPermission = (role, action) => {
                 "view_providers", "view_providers_id", "create_providers", "update_providers", "delete_providers",
                 "view_purchases", "view_purchases_id", "create_purchases", "update_purchases", "delete_purchases",
                 "view_branches","view_branches","create_branches","update_branches","delete_branches", "view_customers",
-                "reate_customers","update_customers","delete_customers","view_quotations","create_quotations",
-                "update_quotations","delete_quotations","view_sales_report","view_top_customers","view_top_products"],
+                "view_customers", "view_customers_id", "create_customers","update_customers","delete_customers",
+                "view_sales","view_sales_id", "create_sales","update_sales", "delete_sales"],
         assistant: ["view_roles", "create_users", "view_users", "view_users_id", "update_users",
                 "view_categories", "create_categories", "update_categories","view_customers",
                 "view_products", "create_products", "update_products", "delete_products",
                 "view_providers", "view_providers_id", "create_providers", "update_providers",
                 "view_purchases", "view_purchases_id", "create_purchases", "update_purchases",
-                "reate_customers","update_customers","view_quotations","create_quotations",
-                "update_quotations","view_sales_report","view_top_customers","view_top_products"],
-        employee: ["view_categories", "view_products", "view_customers","view_quotations","view_sales_report","view_top_customers","view_top_products"]
+                "view_customers", "view_customers_id", "create_customers","update_customers",
+                "view_sales","view_sales_id", "create_sales","update_sales"],
+        employee: ["view_categories", "view_products", "view_customers","view_sales", "view_customers_id","create_sales", "update_sales"]
 
     };
 
